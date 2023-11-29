d403b083197f18b0071baf366980c176aa2f82c6
gildemar <34872457+gildemardev@users.noreply.github.com>
2023-11-29 09:17:40 -0300

    :bug: Corrige bug do linter

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function NotFound() {
   return (
-    <div className="prose text-center dark:prose-invert">
+    <div className="prose dark:prose-invert text-center">
       <h2 className="mb-2">Not Found</h2>
       <p>Could not find requested resource.</p>
       <p>
